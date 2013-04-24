d703824846ca55c2387715efc26e480f4a8950ae
Ben&Chris <apprentice@devbootcamp.com>
2013-04-24 11:04:32 -0700

    Basic Auth from scratch

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+
+  attr_accessible :email, :name, :password, :password_confirmation
+
+  has_secure_password
+  validates_presence_of :password, :on => :create
+
+
+end
